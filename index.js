2091869a5318708c0657508e7b0da8f0cb736f91
Maciek <gandalfakaszary@gmail.com>
2021-04-16 09:12:25 +0200

    added

--- index.js
+++ index.js
@@ -12,10 +12,79 @@ buttonPierwszy.addEventListener('click', function() {
     }
 
 })
+
 buttonDrugi.addEventListener('click', function() {
     console.log('test2')
 
-    if (paragraf.innerHTML === 'ORION') {
+    if (paragraf.innerHTML === 'ORION' || paragraf.innerHTML.startsWith('ORI') == true) {
         paragraf.classList.toggle('orange')
+    } else {
+        paragraf.classList.toggle('none')
     }
+    // if (paragraf.innerHTML.startsWith('ORI') == true) {
+    //     paragraf.classList.toggle('orange')
+
+    // }
+
 })
+
+// buttonDrugi.addEventListener('click', function() {
+//     console.log('test2')
+
+//     // if (paragraf.innerHTML === 'ORION') {
+//     //     paragraf.classList.toggle('orange')
+//     // } else {
+//     //     paragraf.classList.toggle('none')
+//     // }
+//     if (paragraf.innerHTML.startsWith('ORI') == true) {
+//         paragraf.classList.toggle('orange')
+
+//     }
+
+// })
+
+
+
+// console.log('maciek'.startsWith('m'))
+
+// if ('maciek'.startsWith('m') === true) {
+//     paragraf.classList.toggle('orange')
+// } else {
+//     paragraf.classList.toggle('none')
+// }
+// console.log('ORION'.startsWith('ORI'))
+// console.log(paragraf.innerHTML.startsWith('ORI'))
+
+/////////////////////////////////////////////
+
+// buttonDrugi.addEventListener('click', function() {
+//     console.log('test2')
+
+//     if (paragraf.innerHTML.startsWith('ORI') == true) {
+//         paragraf.classList.toggle('orange')
+//     }
+// })
+
+/////////////////////////////////////////////
+
+
+
+
+// if ('ORION'.startsWith('ORI') === true) {
+//     paragraf.classList.toggle('orange')
+// } else {
+//     paragraf.classList.toggle('none')
+// }
+
+
+// buttonDrugi.addEventListener('click', function() {
+//             console.log('test2')
+
+//             if (paragraf.innerHTML.startsWith('ORI') == true || paragraf.innerHTML === 'ORION') {
+//                 paragraf.classList.toggle('orange')
+
+//                 // } else {
+//                 //     paragraf.classList.toggle('none')
+//                 // }
+
+//             }
